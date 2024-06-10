3f68b41c1262680b8ec7982ea5af92ed6a8f4378
Rugved Gawas <Rugved Gawas@LAPTOP-O3QGH3B4.local>
2024-06-10 20:50:35 +0530

    yet another commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Typed } from 'react-typed'
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+   <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+   <p className='text-[#00df9a] font-bold p-2'>Designing with Tailwind CSS</p>
+   <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Tailwind with React</h1>
+   <div>
+    <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Powerfull, Fast, Usefull CSS Framework To Design Webpage</p>
+   </div>
+   </div>
+    </div>
+  )
+}
+
+export default Hero
